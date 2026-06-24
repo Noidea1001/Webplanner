a8496f683a952513f7e7c43ec2125c3880d23448
Error404 <khvysal@gmail.com>
2026-06-24 16:25:15 +0700

    fix error

--- obj/Debug/net10.0/WebPlanner.AssemblyInfo.cs
+++ obj/Debug/net10.0/WebPlanner.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebPlanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+260d0487cc29380ffa046265f697aba0a918da95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+884fc9d63cfe6ee1e5e377266ee8f4b595f00d14")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebPlanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebPlanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
